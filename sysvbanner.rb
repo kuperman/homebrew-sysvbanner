1ce62e862f276e60f9e8ccf7e2cef41a398ac94a
kuperman <kuperman@acm.org>
2016-02-26 11:11:38 -0500

    Added caveat and letting the binary stay 'banner'

--- sysvbanner.rb
+++ sysvbanner.rb
@@ -7,14 +7,24 @@ class Sysvbanner < Formula
 
   def install
     system 'make'
-    #bin.install  'banner'
-    #man1.install 'banner.1'
+    bin.install  'banner'
+    man1.install 'banner.1'
     # These really should be renamed, and changed in the manpage
-    bin.install  'banner' => 'sysvbanner'
-    man1.install 'banner.1' => 'sysvbanner.1'
+    #bin.install  'banner' => 'sysvbanner'
+    #man1.install 'banner.1' => 'sysvbanner.1'
+  end
+  def caveats; <<-EOS.undent
+    This version of banner displays horizontally, not vertically.
+    
+    The supplied version of banner is /usr/bin/banner and is designed
+    for printing on accordion folded paper.
+
+    You will want to change your path to have /usr/local/bin come before
+    /usr/bin to invoke 'banner' directly from the command line.
+    EOS
   end
 
   test do
-    system "#{bin}/sysvbanner", "testing", "banner"
+    system "#{bin}/banner", "testing", "banner"
   end
 end
